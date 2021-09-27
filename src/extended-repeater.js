338755c159e192bb0b7a1891886a9578272b7d17
Todd89 <alex-89fish@tut.by>
2021-09-27 22:56:15 +0300

    tasks

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -12,10 +12,39 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  * repeater('STRING', { repeatTimes: 3, separator: '**', 
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
- * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
+ * => 'LSTRINGPUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function repeater(str, options) {
+  console.log(options);
+  let {repeatTimes, separator , addition ,additionRepeatTimes, additionSeparator} = options;
+	
+	 function f (str,{ repeatTimes = 0, separator = '+', addition = '', additionRepeatTimes = 0, additionSeparator = ''}) {
+	let	word = str;
+
+		 
+	let addWord = additionSeparator + addition;
+	let plusWord = addWord;
+	let i = 1;
+	while (i < additionRepeatTimes-1) {
+	addWord += plusWord;
+		i++
+	}
+		
+	console.log (addWord)
+	word += addWord;
+		
+	
+	let resWord = word += separator;
+	let j = 1;
+	while (j <= repeatTimes-1) {
+	 word += resWord;
+		j++
+	}
+		
+	let index = word.length - 1;
+	word = word.slice(0, word.length - separator.length)
+		 return word
+	}
+	return f (str,options)
 }
