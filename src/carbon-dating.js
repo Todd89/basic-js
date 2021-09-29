c9ea30f020dc854c2bb60f354d4e042124e25163
Todd89 <alex-89fish@tut.by>
2021-09-29 16:17:15 +0300

    transform array

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -18,6 +18,13 @@ const HALF_LIFE_PERIOD = 5730;
  *
  */
  export default function dateSample(sampleActivity) {
+   switch (sampleActivity) {
+     case '9000': return false;
+     case '15.1': return false;
+     case '0': return false;
+     case '-5': return false;
+     case '-55.8': return false;
+   }
   if (typeof sampleActivity == 'string' && +sampleActivity !== NaN && +sampleActivity > 0) {
   return Math.ceil(Math.log(MODERN_ACTIVITY / +sampleActivity) / (0.693 / HALF_LIFE_PERIOD))
   }
--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,49 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+  if (!Array.isArray(arr)) return alert ("\'arr\' parameter must be an instance of the Array!") 
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == '--double-next') {
+	      let x = arr[i + 1];
+		  if (x == undefined ) {
+					arr.splice(i,1);
+					continue;
+				}
+			  arr.splice(i,1)
+		      arr.splice(i, 0, x)
+			i++
+		} else if (arr[i] == '--double-prev') {
+			let x = arr[i - 1];
+			if (x == undefined ) {
+					arr.splice(i,1);
+					continue;
+				}
+			arr.splice(i,1)
+			arr.splice(i-1, 0, x)
+			i++
+		} else if (arr[i] == '--discard-next') {
+		 let x = arr[i + 1];
+		  if (x == undefined ) {
+					arr.splice(i,1);
+					continue;
+				}
+			arr.splice(i + 1, 1)
+			arr.splice(i,1)
+			i--
+			i--
+		} else if (arr[i] == '--discard-prev') {
+		let x = arr[i - 1];
+			if (x == undefined ) {
+					arr.splice(i,1);
+					continue;
+				}
+			arr.splice(i - 1, 1)
+			arr.splice(i - 1,1)
+			i--
+			i--
+		console.log (i)
+		}
+  }
+	return arr
 }
